56ab4da5424d7895940273a25247e6b47187e439
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-04-09 15:16:51 -0400

    committing

--- js/incrementLastArrayElements.js
+++
@@ -1,9 +0,0 @@
-function incrementLastArrayElement(_array)  {
-    var newArray = [];
-    newArray = _array.slice(0);
-    var lastNumber = newArray.pop();
-    newArray.push(lastNumber + 1);
-    return newArray;
-}
-
-//toy problems can be annoying
